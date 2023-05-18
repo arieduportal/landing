224ba2e6e89db6b9ff550f4cf4111343cdbc3840
arinzejustin <arinzejustinng@gmail.com>
2023-05-18 17:18:25 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex h-screen flex-col items-center justify-between p-0">
       <div className="fixed transform -translate-y-1/2 top-1/2">
         <Image src="" alt="" className='hidden'></Image>
-        <p className="text-center font-bold uppercase text-2xl">Website is currently undergoing update</p>
+        <p className="text-center font-bold uppercase text-2xl">Website is currently undergoing updates</p>
       </div>
     </main>
   )

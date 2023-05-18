1ef514bd4e6492f4bc62e5849f6fd16578b52461
arinzejustin <arinzejustinng@gmail.com>
2023-05-18 17:04:50 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-between p-24 bg-white">
-      
+    <main className="flex h-screen flex-col items-center justify-between p-0">
+      <div className="fixed transform -translate-y-1/2 top-1/2">
+        <Image src="" alt="" className='hidden'></Image>
+        <p className="text-center font-bold uppercase text-2xl">Website is currently undergoing update</p>
+      </div>
     </main>
   )
 }

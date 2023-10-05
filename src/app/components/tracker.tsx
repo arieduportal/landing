7f9fe08ad6d45600ee83fa30e1ab6b6e7718b9d7
arinzejustin <arinzejustinng@gmail.com>
2023-10-06 00:34:36 +0100

    commit

---
+++ src/app/components/tracker.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useEffect } from 'react';
+
+export default function Tracker() {
+
+
+    useEffect(() => {
+        const getDate = () => {
+            const t = new Date(),
+                date = ("0" + t.getDate()).slice(-2),
+                month = ("0" + (t.getMonth() + 1)).slice(-2),
+                year = t.getFullYear();
+            return `${date}.${month}.${year}`;
+        }
+
+        const tracker = (
+            t: Window & typeof globalThis,
+            r: Document,
+            a: string,
+            c: string,
+            k?: string,
+            e?: {
+                async: boolean;
+                src: string;
+                setAttribute: (arg0: any, arg1: string) => void;
+                crossOrigin: string;
+                defer: boolean;
+            },
+            d?: string,
+            y?: { parentNode: { insertBefore: (arg0: any, arg1: any) => void } }
+        ) => {
+            d = window.location.hostname;
+            k = getDate();
+            //@ts-ignore
+            e = r.createElement(a);
+            e!.async = true;
+            const version = k.split('.');
+            //@ts-ignore
+            e.src = `https://static.arieducationportal.com/ajax/js/tracker[${version[0]}.${version[1]}]?s=${d}/?id=${t.TRACK_ID}`;
+            e!.setAttribute(
+                c,
+                //@ts-ignore
+                `{v: [${version[0]}.${version[1]}], date: ${k}, tracker: ${t.TRACK_ID}}`
+            );
+            e!.crossOrigin = "use-credentials";
+            e!.defer = true;
+            //@ts-ignore
+            y = r.getElementsByTagName(a)[1];
+            y!.parentNode.insertBefore(e, y);
+        }
+        setTimeout(() => {
+            tracker(window, document, "script", "data-ae-access");
+        }, 7000)
+    }, []);
+
+    return (
+        <div></div>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Script from 'next/script';
+import { Providers } from "./providers";
+import Tracker from "@/app/components/tracker"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Best Information And Result Management System In Nigeria | arieducationportal.com',
-  description: 'Best Information And Result Management System In Nigeria',
+  title: 'School Information Management System | arieducationportal.com',
+  description: 'Streamline academic data organization, track student proformance, manage grades, attendence, and examination results effortlessly',
 }
 
 export default function RootLayout({
@@ -17,15 +19,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Script strategy="beforeInteractive" id="tracker_id">
+        <Script strategy="afterInteractive" id="tracker_id">
           {`
-          window.TRACK_ID = "AE_EF69E-19F66-F7D2F328-0AF1";
+          window.TRACK_ID = "AE_1B267-619C4-812CC46E-E281";
         `}
         </Script>
         <main id="_next">
-          {children}
+          <Providers>
+            {children}
+          </Providers>
+          <Tracker />
         </main>
-        <Script src="/js/track.js" strategy="afterInteractive" />
       </body>
     </html>
   )

8a1336ed138c31976ed9d4e3d3903c781d38dc45
arinzejustin <arinzejustinng@gmail.com>
2024-04-09 05:12:35 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,34 @@ const socials = [
 export default function Index() {
   return (
     <div className="min-h-[200vh]">
-      <div className="text-lg uppercase text-black mt-3">
-        hello world Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque recusandae adipisci animi ab similique dolore est ipsam culpa harum explicabo magnam minima rem dicta exercitationem a cupiditate, voluptatum in eligendi.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto fugit laboriosam velit iusto, non temporibus cum atque officiis! Explicabo id quisquam inventore est corrupti provident, dignissimos ipsa asperiores officia nam!
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi ipsam natus libero ducimus in? Vel ipsum tempore expedita soluta! Autem, illum officia quisquam reiciendis consectetur quam ipsam maxime quod molestias.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, reprehenderit velit, repellat molestias necessitatibus aperiam illum odit nisi natus distinctio iusto expedita ea eos explicabo quis, omnis magnam voluptatem perferendis?
+      <div className="">
+
+      </div>
+      <div className="my-3 py-3">
+        <div className="app-container flex justify-center items-center mx-auto relative">
+          <div className="flex relative flex-wrap min-h-[1px] flex-col justify-center items-center align-middle">
+            <div className="text-center">
+              <div className="rounded-full bg-sky-200 text-sky-700 px-4 py-1">
+                <p className="uppercase text-sm text-sky-700 px-1 py-0.5 font-roboto">our services</p>
+              </div>
+            </div>
+            <div className="text-center mt-2 pt-2 mb-4 pb-4 w-full md:w-3/4 lg:w-2/3">
+              <p className="text-teal-700 font-roboto text-5xl font-semibold leading-[1.3]">We develop solutions for schools</p>
+            </div>
+          </div>
+        </div>
+        <div className="pt-4">
+          <div className="pt-20abcdefghijklmnopqrstuvwxyz  bg-gradient-to-b from-slate-200 from-65% to-slate-50">
+            <div className="-mt-28 app-container gap-2 md:gap-4 grid grid-cols-1 md:grid-cols-2 justify-between align-middle items-center">
+              <div className="bg-white shadow-lg rounded-md py-4 px-2.5 text-slate-700 text-sm flex m-2"></div>
+              <div className="bg-white shadow-lg rounded-md py-4 px-2.5 text-slate-700 text-sm flex m-2"></div>
+              <div className="bg-white shadow-lg rounded-md py-4 px-2.5 text-slate-700 text-sm flex m-2"></div>
+              <div className="bg-white shadow-lg rounded-md py-4 px-2.5 text-slate-700 text-sm flex m-2"></div>
+              <div className="bg-white shadow-lg rounded-md py-4 px-2.5 text-slate-700 text-sm flex m-2"></div>
+              <div className="bg-white shadow-lg rounded-md py-4 px-2.5 text-slate-700 text-sm flex m-2"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )

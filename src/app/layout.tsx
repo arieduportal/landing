316b9b757e98a6e7cf62b57e60e03e1bd640a725
arinzejustin <arinzejustinng@gmail.com>
2024-07-12 14:00:58 +0100

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import './globals.css'
 import './animate.css'
 
 export const metadata = {
-  title: 'School Information Management System | arieducationportal.com',
+  title: 'School Information Management System | Axiolot Hub',
   description: 'Manage all aspects of your school with ease. Our app includes an Academics Portal for accessing resources, an ePayment Portal for online transactions, an Analytics Portal for data-driven decisions, Site Management tools for website control, and a Staff Payment System for efficient payroll management. Streamline your school operations today!',
 }
 
@@ -26,20 +26,24 @@ export default function RootLayout({
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link rel="preconnect" href="https://static.arieducationportal.com" />
+        <link rel="preconnect" href="https://static.axiolot.com.ng" />
         <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
-        <link rel="dns-prefetch" href="https://static.arieducationportal.com" />
+        <link rel="dns-prefetch" href="https://static.axiolot.com.ng" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" sizes="16x16" href="https://static.arieducationportal.com/image/icons/16.png" type="image/png" />
-        <link rel="icon" sizes="32x32" href="https://static.arieducationportal.com/image/icons/32.png" type="image/png" />
-        <link rel="icon" sizes="96x96" href="https://static.arieducationportal.com/image/icons/96.png" type="image/png" />
-        <link rel="apple-touch-icon" sizes="167x167" href="https://static.arieducationportal.com/image/icons/167.png" type="image/png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="https://static.arieducationportal.com/image/icons/152.png" type="image/png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="https://static.arieducationportal.com/image/icons/180.png" type="image/png" />
-        <link rel="apple-touch-icon" href="https://static.arieducationportal.com/image/icons/120.png" type="image/png" />
+        <link rel="icon" sizes="16x16" href="https://static.axiolot.com.ng/image/icons/16.png" type="image/png" />
+        <link rel="icon" sizes="32x32" href="https://static.axiolot.com.ng/image/icons/32.png" type="image/png" />
+        <link rel="icon" sizes="96x96" href="https://static.axiolot.com.ng/image/icons/96.png" type="image/png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="https://static.axiolot.com.ng/image/icons/167.png" type="image/png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="https://static.axiolot.com.ng/image/icons/152.png" type="image/png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://static.axiolot.com.ng/image/icons/180.png" type="image/png" />
+        <link rel="apple-touch-icon" href="https://static.axiolot.com.ng/image/icons/120.png" type="image/png" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Roboto&family=Inter&family=Merriweather&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.cdnfonts.com/css/satoshi"
         />
       </head>
       <body className="font-roboto">
